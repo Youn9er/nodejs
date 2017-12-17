e70baa1ba41d189a366515ce3d8a5590d020a850
刚 <984805129@qq.com>
2017-12-17 23:26:17 +0800

    url.parse方法

---
+++ http-data/http-get/server-url.js
@@ -0,0 +1,19 @@
+const http=require('http');
+const urlLib=require('url');
+
+//url.parse():将一个URL字符串转换成对象并返回。
+http.createServer(function (req, res){
+  var obj=urlLib.parse(req.url, true);
+  
+  /*false，//foo/bar 形式的字符串将被解释成 { pathname: ‘//foo/bar' }
+    true，//foo/bar 形式的字符串将被解释成  { host: ‘foo', pathname: ‘/bar' }*/
+
+  var url=obj.pathname;
+  var GET=obj.query;
+
+  console.log(url, GET);
+
+  //req获取前台请求数据
+  res.write('aaa');
+  res.end();
+}).listen(8889);
